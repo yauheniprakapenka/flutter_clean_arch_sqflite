52c40ca0bd9dfad899bbd04396c5af60f4893fd8
Yauheni Prakapenka <yauheni.prakapenka@gmail.com>
2021-12-17 10:33:55 +0300

    challenge: add image button

--- app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -4,13 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
-    private lateinit var nextButton: Button
+    private lateinit var nextButton: ImageButton
     private lateinit var previousButton: Button
     private lateinit var questionTextView: TextView
 
