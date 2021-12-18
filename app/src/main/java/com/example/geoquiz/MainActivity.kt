529c3d804e8672c9beb20cff48534508449702a1
Yauheni Prakapenka <yauheni.prakapenka@gmail.com>
2021-12-18 14:12:47 +0300

    Add QuizViewModel

--- app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -7,7 +7,7 @@ import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
-import kotlin.math.log
+import androidx.lifecycle.ViewModelProviders
 
 private const val TAG = "MainActivity"
 
@@ -18,93 +18,55 @@ class MainActivity : AppCompatActivity() {
     private lateinit var previousButton: Button
     private lateinit var questionTextView: TextView
 
-    private val questionBank = listOf(
-        Question(R.string.question_australia, true),
-        Question(R.string.question_oceans, true),
-        Question(R.string.question_mideast, false),
-//        Question(R.string.question_africa, false),
-//        Question(R.string.question_americas, true),
-//        Question(R.string.question_asia, true),
-    )
+    private val quizViewModel: QuizViewModel by lazy {
+        ViewModelProviders.of(this).get(QuizViewModel::class.java)
+    }
 
-    private var currentIndex = 0
-    private var selectedAnswer: Boolean? = null
+    private fun showResult() {
+        val correctAnswersPerPercent = quizViewModel.getResult()
+        Toast.makeText(this, "Правильных ответов: $correctAnswersPerPercent%", Toast.LENGTH_SHORT)
+            .show()
+    }
+
+    private fun updateQuestion() {
+        val questionTextResId = quizViewModel.getResourceId()
+        val questionString = resources.getString(
+            resources.getIdentifier(
+                questionTextResId.toString(),
+                "string",
+                packageName
+            )
+        )
+        questionTextView.text = questionString
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_main)
         findViewsById()
-
         trueButton.setOnClickListener { view: View ->
-            onTrueButtonClicked()
+            quizViewModel.onTrueButtonClicked()
         }
 
         falseButton.setOnClickListener { view: View ->
-            onFalseButtonClicked()
+            quizViewModel.onFalseButtonClicked()
         }
 
         nextButton.setOnClickListener { view: View ->
-            val nextQuestionIndex = getNextQuestionIndex(questionBank.count(), currentIndex)
-            if (nextQuestionIndex != null) {
-                currentIndex = nextQuestionIndex
+            val hasNextQuestion = quizViewModel.hasNextQuestion()
+            if (hasNextQuestion) {
                 updateQuestion()
             } else {
                 showResult()
             }
         }
 
-        questionTextView.setOnClickListener { view: View ->
-            currentIndex += 1
-            updateQuestion()
-        }
-
         previousButton.setOnClickListener { view: View ->
-            currentIndex -= 1
+            quizViewModel.onPreviousButtonClicked()
             updateQuestion()
         }
 
         updateQuestion()
-    }
-
-    private fun showResult() {
-        val totalQuestions = questionBank.count()
-        val correctAnswers = countCorrectAnswers(questionBank)
-        val correctAnswersPerPercent = getPercentFromNumber(totalNumbers = totalQuestions, fromNumber = correctAnswers)
-        Toast.makeText(this, "Правильных ответов: $correctAnswersPerPercent%", Toast.LENGTH_SHORT).show()
-    }
-
-    private fun updateQuestion() {
-        val questionTextResId = questionBank[currentIndex].textResId
-        val questionNumber = currentIndex + 1
-        val questionString = resources.getString(resources.getIdentifier(questionTextResId.toString(), "string", packageName))
-        questionTextView.text = "$questionNumber. $questionString"
-    }
-
-    private fun onTrueButtonClicked() {
-        if (selectedAnswer == null) {
-            val result = true
-            selectedAnswer = result
-            questionBank[currentIndex].userAnswer = result
-        }
-    }
-
-    private fun onFalseButtonClicked() {
-        if (selectedAnswer == null) {
-            val result = false
-            selectedAnswer = result
-            questionBank[currentIndex].userAnswer = result
-        }
-    }
-
-    // Returns [null] if no more questions.
-    private fun getNextQuestionIndex(totalQuestions: Int, currentQuestion: Int): Int? {
-        val formattedTotalQuestions = totalQuestions - 1
-        if (currentQuestion < formattedTotalQuestions) {
-            clearUserAnswer()
-            return currentQuestion + 1
-        }
-        return null
     }
 
     private fun findViewsById() {
@@ -114,24 +76,4 @@ class MainActivity : AppCompatActivity() {
         previousButton = findViewById(R.id.previous_button)
         questionTextView = findViewById(R.id.question_text_view)
     }
-
-    private fun clearUserAnswer() {
-        selectedAnswer = null
-    }
-
-    private fun countCorrectAnswers(questionBank: List<Question>): Int {
-        var rightAnswers = 0
-        for (question in questionBank) {
-            if (question.answer == question.userAnswer) {
-                rightAnswers++
-            }
-        }
-        return rightAnswers
-    }
-
-    private fun getPercentFromNumber(totalNumbers: Int, fromNumber: Int): Int {
-        val totalPercent = 100
-        return (totalPercent * fromNumber) / totalNumbers
-    }
-
 }
---
+++ app/src/main/java/com/example/geoquiz/QuizViewModel.kt
@@ -0,0 +1,90 @@
+package com.example.geoquiz
+
+import androidx.lifecycle.ViewModel
+
+//private const val TAG = "QuizViewModel"
+
+class QuizViewModel : ViewModel() {
+    private val questionBank = listOf(
+        Question(R.string.question_australia, true),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_mideast, false),
+//        Question(R.string.question_africa, false),
+//        Question(R.string.question_americas, true),
+//        Question(R.string.question_asia, true),
+    )
+
+    private var currentIndex = 0
+    private var selectedAnswer: Boolean? = null
+
+    fun getResult(): Int {
+        val totalQuestions = questionBank.count()
+        val correctAnswers = countCorrectAnswers(questionBank)
+        return getPercentFromNumber(totalNumbers = totalQuestions, fromNumber = correctAnswers)
+    }
+
+    fun getResourceId(): Int {
+        return questionBank[currentIndex].textResId
+    }
+
+    fun onTrueButtonClicked() {
+        if (selectedAnswer == null) {
+            val result = true
+            selectedAnswer = result
+            questionBank[currentIndex].userAnswer = result
+        }
+    }
+
+    fun onFalseButtonClicked() {
+        if (selectedAnswer == null) {
+            val result = false
+            selectedAnswer = result
+            questionBank[currentIndex].userAnswer = result
+        }
+    }
+
+    fun onPreviousButtonClicked() {
+        currentIndex -= 1
+    }
+
+    fun hasNextQuestion(): Boolean {
+        val nextQuestionIndex = getNextQuestionIndex()
+        if (nextQuestionIndex != null) {
+            currentIndex = nextQuestionIndex
+            return true
+        }
+        return false
+    }
+
+    // Returns [null] if no more questions.
+    private fun getNextQuestionIndex(): Int? {
+        val totalQuestions = questionBank.count()
+        val currentQuestion = currentIndex
+        val formattedTotalQuestions = totalQuestions - 1
+        if (currentQuestion < formattedTotalQuestions) {
+            clearUserAnswer()
+            return currentQuestion + 1
+        }
+        return null
+    }
+
+    private fun clearUserAnswer() {
+        selectedAnswer = null
+    }
+
+    private fun countCorrectAnswers(questionBank: List<Question>): Int {
+        var rightAnswers = 0
+        for (question in questionBank) {
+            if (question.answer == question.userAnswer) {
+                rightAnswers++
+            }
+        }
+        return rightAnswers
+    }
+
+    private fun getPercentFromNumber(totalNumbers: Int, fromNumber: Int): Int {
+        val totalPercent = 100
+        return (totalPercent * fromNumber) / totalNumbers
+    }
+
+}
