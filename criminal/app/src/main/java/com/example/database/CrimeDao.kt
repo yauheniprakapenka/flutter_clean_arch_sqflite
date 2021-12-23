3f81ecebd383a8afca1aef397dc176ef4ab57623
Yauheni Prakapenka <yauheni.prakapenka@gmail.com>
2021-12-23 16:48:54 +0300

    [crime] add room, add crime repository

---
+++ criminal/app/src/main/java/com/example/database/CrimeDao.kt
@@ -0,0 +1,15 @@
+package com.example.database
+
+import androidx.room.Dao
+import androidx.room.Query
+import com.example.criminal.Crime
+import java.util.*
+
+@Dao
+interface CrimeDao {
+    @Query("SELECT * FROM crime")
+    fun getCrimes(): List<Crime>
+
+    @Query("SELECT * FROM crime WHERE id=(:id)")
+    fun getCrime(id: UUID): Crime?
+}
---
+++ criminal/app/src/main/java/com/example/database/CrimeRepository.kt
@@ -0,0 +1,20 @@
+package com.example.database
+
+import android.content.Context
+import java.lang.IllegalStateException
+
+class CrimeRepository private constructor(context: Context) {
+    companion object {
+        private var INSTANCE: CrimeRepository? = null
+
+        fun initialize(context: Context) {
+            if (INSTANCE == null) {
+                INSTANCE = CrimeRepository(context)
+            }
+        }
+
+        fun get(): CrimeRepository {
+            return INSTANCE ?: throw IllegalStateException("CrimeRepository must be initialized")
+        }
+    }
+}
