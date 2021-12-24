d42ffc7c5e88f9c3c4b24c88660297e10aa21a81
Yauheni Prakapenka <yauheni.prakapenka@gmail.com>
2021-12-24 13:19:16 +0300

    [crime] add transition to crime details

--- criminal/app/src/main/java/com/example/criminal/CrimeListFragment.kt
+++ criminal/app/src/main/java/com/example/criminal/CrimeListFragment.kt
@@ -1,5 +1,6 @@
 package com.example.criminal
 
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -13,15 +14,27 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import java.util.*
+import javax.security.auth.callback.Callback
 
 private const val TAG = "CrimeListFragment"
 
 class CrimeListFragment : Fragment() {
+    interface Callbacks {
+        fun onCrimeSelected(crimeId: UUID)
+    }
+
+    private var callbacks: Callbacks? = null
     private lateinit var crimeRecyclerView: RecyclerView
     private var adapter: CrimeAdapter? = CrimeAdapter(emptyList())
 
     private val crimeListViewModel: CrimeListViewModel by lazy {
         ViewModelProviders.of(this).get(CrimeListViewModel::class.java)
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        callbacks = context as Callbacks?
     }
 
     override fun onCreateView(
@@ -47,6 +60,11 @@ class CrimeListFragment : Fragment() {
             })
     }
 
+    override fun onDetach() {
+        super.onDetach()
+        callbacks = null
+    }
+
     private inner class CrimeHolder(view: View) : RecyclerView.ViewHolder(view),
         View.OnClickListener {
         private lateinit var crime: Crime
@@ -69,7 +87,7 @@ class CrimeListFragment : Fragment() {
         }
 
         override fun onClick(v: View?) {
-            Toast.makeText(context, "${crime.title} pressed", Toast.LENGTH_SHORT).show()
+            callbacks?.onCrimeSelected(crime.id)
         }
     }
 
--- criminal/app/src/main/java/com/example/criminal/MainActivity.kt
+++ criminal/app/src/main/java/com/example/criminal/MainActivity.kt
@@ -2,8 +2,12 @@ package com.example.criminal
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import java.util.*
 
-class MainActivity : AppCompatActivity() {
+private const val TAG = "MainActivity"
+
+class MainActivity : AppCompatActivity(), CrimeListFragment.Callbacks {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -11,11 +15,23 @@ class MainActivity : AppCompatActivity() {
         addCrimeFragment()
     }
 
+    override fun onCrimeSelected(crimeId: UUID) {
+        Log.d(TAG, "Mainactivity.onCrimeSelected: $crimeId")
+        val fragment = CrimeFragment()
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .addToBackStack(null)
+            .commit()
+    }
+
     private fun addCrimeFragment() {
         val currentFragment = supportFragmentManager.findFragmentById(R.id.fragment_container)
         if (currentFragment == null) {
-            val fragment = CrimeListFragment.newInstance()
-            supportFragmentManager.beginTransaction().add(R.id.fragment_container, fragment)
+            val fragment = CrimeListFragment()
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.fragment_container, fragment)
                 .commit()
         }
     }
