6a6d7a3a33f8460c58df8f3e3444b569bd743467
Yauheni Prakapenka <yauheni.prakapenka@gmail.com>
2021-12-27 14:40:38 +0300

    [crime] add create new item crime button

--- criminal/app/src/main/java/com/example/criminal/CrimeListFragment.kt
+++ criminal/app/src/main/java/com/example/criminal/CrimeListFragment.kt
@@ -2,9 +2,7 @@ package com.example.criminal
 
 import android.content.Context
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
@@ -35,6 +33,11 @@ class CrimeListFragment : Fragment() {
         callbacks = context as Callbacks?
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -62,6 +65,24 @@ class CrimeListFragment : Fragment() {
     override fun onDetach() {
         super.onDetach()
         callbacks = null
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.fragment_crime_list, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.new_crime -> {
+                val crime = Crime()
+                crimeListViewModel.addCrime(crime)
+                callbacks?.onCrimeSelected(crime.id)
+                true
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
+
     }
 
     private inner class CrimeHolder(view: View) : RecyclerView.ViewHolder(view),
--- criminal/app/src/main/java/com/example/criminal/CrimeListViewModel.kt
+++ criminal/app/src/main/java/com/example/criminal/CrimeListViewModel.kt
@@ -6,24 +6,6 @@ import com.example.database.CrimeRepository
 import java.util.*
 
 class CrimeListViewModel: ViewModel() {
-//    val crimes = mutableListOf<Crime>()
-//
-//    init {
-//        crimes.addAll(addCrimes())
-//    }
-//
-//    private fun addCrimes(): MutableList<Crime> {
-//        val crimes = mutableListOf<Crime>()
-//        for (i in 0 until 100) {
-//            val crime = Crime()
-//            crime.title = "Crime #$i"
-//            crime.isSolved = i % 2 == 0
-//            crime.date = Date(1563781647000)
-//            crimes += crime
-//        }
-//        return crimes
-//    }
-
     private val crimeRepository = CrimeRepository.get()
 
     val crimeListLiveData = crimeRepository.getCrimes()
@@ -32,5 +14,9 @@ class CrimeListViewModel: ViewModel() {
         // Solution: https://www.geeksforgeeks.org/date-and-time-formatting-in-android/
         val format = "EEEE, LLL dd, yyyy"
         return DateFormat.format(format, date).toString()
+    }
+
+    fun addCrime(crime: Crime) {
+        crimeRepository.addCrime(crime)
     }
 }
